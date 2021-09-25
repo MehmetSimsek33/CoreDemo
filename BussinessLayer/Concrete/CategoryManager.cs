8d59334b280b1fa83890d7c5a45a8d23607db466
Mehmet şimşek <Mehmetsimsek3212368@hotmail.com>
2021-09-25 21:50:10 +0300

    Add project files.

---
+++ BussinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,47 @@
+﻿using BussinessLayer.Abstarct;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFrameWork;
+using EntityLayer.ConcrEte;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EfCategoryRepository efCategoryRepository;
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            efCategoryRepository = new EfCategoryRepository();
+        }
+
+        public void CategoryAdd(Category category)
+        {
+            efCategoryRepository.Insert(category);
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            efCategoryRepository.Delete(category);
+
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            efCategoryRepository.Update(category);
+        }
+
+        public List<Category> GetAll()
+        {
+            return efCategoryRepository.GetAll();
+        }
+
+        public Category GetById(int id)
+        {
+            return efCategoryRepository.GetById(id);
+        }
+    }
+}
---
+++ EntityLayer/ConcrEte/Blog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.ConcrEte
+{
+   public class Blog
+    {
+        [Key]
+        public int BlogId { get; set; }
+        public string Title { get; set; }
+        public string BlogContent { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string Status { get; set; }
+        public string ThumbnailImage { get; set; }
+        public int Image { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public List<Comment> Comments { get; set; }
+    }
+}
